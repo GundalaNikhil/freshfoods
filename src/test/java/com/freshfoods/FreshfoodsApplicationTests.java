e740051ba27aa2aa73164078997acd77862c5746
Nikhil <nikhil.gundala@fedex.com>
2024-10-28 00:12:53 +0530

    SetUp

---
+++ src/test/java/com/freshfoods/FreshfoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshfoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshfoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
